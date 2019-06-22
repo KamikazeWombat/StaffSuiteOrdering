054c5c27334cf8c90462bfc331db3f4c8a894569
Wombat3 <benmillerhnd@gmail.com>
2019-06-22 00:07:10 -0400

    Adjustments to basic code to do an API request, first stages of poking at json

--- testfile.py
+++ testfile.py
@@ -1,11 +1,15 @@
-import sys
+#import sys
 import requests
+import json
 
 API_ENDPOINT = "https://staging-reggie.magfest.org/jsonrpc/"
-API_KEY = "014ef08b-fa5e-438a-b89c-c7d6c6f7c43a"
-REQUEST_HEADERS = {'X-Auth-Token':'014ef08b-fa5e-438a-b89c-c7d6c6f7c43a'}
+
+#read in authentication toke from file instead of hardcoded.  security yall!
+authfile = open('authtoken.cfg', 'r')
+REQUEST_HEADERS = {'X-Auth-Token':authfile.read()}
 #data being sent to API
 request_data = {'method':'attendee.search',
                'params':['Test Developer']}
 request = requests.post(url = API_ENDPOINT, json = request_data, headers = REQUEST_HEADERS)
+#testprint = json.loads(request.text, sort_keys=True, indent=2)
 print(request.text)
