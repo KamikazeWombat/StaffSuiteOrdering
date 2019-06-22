58c1de927b6af6a43b9bf5c831aaaec683ac1f75
Wombat3 <benmillerhnd@gmail.com>
2019-06-21 23:30:27 -0400

    Got it to get an API response from staging Reggie!!!

--- testfile.py
+++ testfile.py
@@ -1,1 +1,11 @@
 import sys
+import requests
+
+API_ENDPOINT = "https://staging-reggie.magfest.org/jsonrpc/"
+API_KEY = "014ef08b-fa5e-438a-b89c-c7d6c6f7c43a"
+REQUEST_HEADERS = {'X-Auth-Token':'014ef08b-fa5e-438a-b89c-c7d6c6f7c43a'}
+#data being sent to API
+request_data = {'method':'attendee.search',
+               'params':['Test Developer']}
+request = requests.post(url = API_ENDPOINT, json = request_data, headers = REQUEST_HEADERS)
+print(request.text)
