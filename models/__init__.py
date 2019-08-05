3c15271d8f814e5e02db7ac5d7e4dac6f842a527
Wombat3 <benmillerhnd@gmail.com>
2019-08-05 00:02:04 -0400

    cleaned up a bit, created branch to work in

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,14 @@
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
 
 from config import cfg, dec_base
 from models import meal, attendee, order
 
 engine = create_engine(cfg.database_location)
 
-print(dec_base.metadata)
+
 dec_base.metadata.create_all(bind=engine)
-print('just created tables')
-Session = sessionmaker(bind=engine)
-session = Session(bind=engine)
+
+
+
 
