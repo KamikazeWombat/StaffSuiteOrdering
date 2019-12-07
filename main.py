92457803f8664967d48e69924b0a5580b2712e19
Wombat3 <benmillerhnd@gmail.com>
2019-12-07 11:03:33 -0500

    Enable HTTPS and redirect from HTTP to HTTPS
    Need to be secure as part of preventing data destruction
    CSRF not reliable unless HHTPS is used
    
    Added certificate and configured cherrypy to use it for https
    Added a second cherrypy server to listen for http requests and redirect to https

--- main.py
+++ main.py
@@ -5,9 +5,31 @@ from config import cfg, c
 from shared_functions import load_departments
 import webcode
 
+# force_tls and load_http_server both copied from this guy's blog post.  thanks much for showing me how to do this!
+# http://www.fcollyer.com/posts/cherrypy-only-http-and-https-app-serving/
+
+
+def force_tls():
+    if cherrypy.request.scheme == "http":
+        # see https://support.google.com/webmasters/answer/6073543?hl=en
+        raise cherrypy.HTTPRedirect(cherrypy.url().replace("http:", "https:"),
+                                    status=301)
+
+
+cherrypy.tools.force_tls = cherrypy.Tool("before_handler", force_tls)
+
+
+def load_http_server():
+    # extra server instance to redirect HTTP requests to HTTPS
+    server = cherrypy._cpserver.Server()
+    # server.socket_host = "0.0.0.0"
+    server.socket_port = 80
+    server.subscribe()
+
 
 def main():
     load_departments()
+    load_http_server()
     cherrypy.quickstart(webcode.Root(), '/', cfg.cherrypy)
 
 
