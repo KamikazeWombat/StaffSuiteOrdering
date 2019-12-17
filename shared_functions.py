ce4a668937aaf2170535839ebeabb27cd7cf555f
Wombat3 <benmillerhnd@gmail.com>
2019-12-16 22:52:51 -0500

    Add code for troubleshooting webhook stuff
    Shows the response below the Update button after sending test.

--- shared_functions.py
+++ shared_functions.py
@@ -637,4 +637,8 @@ def create_dept_order(dept_id, meal_id, session):
 
 def send_webhook(url, data):
     request = requests.post(url=url, json=data)
-    return
+    response = json.loads(request.text)
+    #if 'error' in response:
+    print('---------------------------------')
+    print(response)
+    return response
