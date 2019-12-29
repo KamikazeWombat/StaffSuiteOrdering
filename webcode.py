8049db539be520397c14fb16e7daf1028282fa48
Wombat3 <benmillerhnd@gmail.com>
2019-12-28 23:42:50 -0500

    Add thing for potentially dangerous stuff
    Added a thing for potentially dangerous operations
    put Reset Department List in it for now.

--- webcode.py
+++ webcode.py
@@ -694,7 +694,7 @@ class Root:
 
     @cherrypy.expose
     @admin_req
-    def config(self, message=[], **params):
+    def config(self, message=[], dangerous=False, **params):
         messages = []
 
         if message:
@@ -737,8 +737,27 @@ class Root:
                                session=session_info,
                                admin_list=admin_list,
                                staffer_list=staffer_list,
+                               dangerous=dangerous,
                                c=c,
                                cfg=cfg)
+
+    @cherrypy.expose
+    @admin_req
+    def dangerous(self, reset_dept_list=False):
+        """
+        For hidden buttons to do potentially very dangerous things
+        """
+        session = models.new_sesh()
+        if reset_dept_list:
+            depts = session.query(Department).all()
+            for dept in depts:
+                session.delete(dept)
+            session.commit()
+            shared_functions.load_departments()
+        
+        session.close()
+        raise HTTPRedirect("config")
+        
 
     @cherrypy.expose
     @dh_or_admin
