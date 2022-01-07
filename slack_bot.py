ac18f0ce470b9140d90888677dffe7b0a07086fa
KamikazeWombat <benmillerhnd@gmail.com>
2022-01-06 21:58:34 -0500

    Now sends me Slack messages when it has a problem sending a Slack message

--- slack_bot.py
+++ slack_bot.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from config import cfg
@@ -14,17 +15,26 @@ def send_message(channel, message):
 
     channel_list = channel.split(',')
     for chan in channel_list:
-        if chan and chan[0] == '@':
-            open_channel = {'token': cfg.slack_authkey,
-                            'users': chan}
-            response = requests.post('https://slack.com/api/conversations.open', open_channel, headers)
+        try:
+            if chan and chan[0] == '@':
+                open_channel = {'token': cfg.slack_authkey,
+                                'users': chan}
+                response = requests.post('https://slack.com/api/conversations.open', open_channel, headers)
+                if not json.loads(response.text)['ok']:
+                    send_message("@Wombat3", "Error sending Slack message to " + chan + ' - \r\n' + response.text + ' @wombat3')
 
-        data = {'token': cfg.slack_authkey,
-                'link_names': 'true',
-                'unfurl_links': 'false',
-                'channel': chan,
-                'text': message}
+            data = {'token': cfg.slack_authkey,
+                    'link_names': 'true',
+                    'unfurl_links': 'false',
+                    'channel': chan,
+                    'text': message}
 
-        response = requests.post('https://slack.com/api/chat.postMessage', data, headers)
+            response = requests.post('https://slack.com/api/chat.postMessage', data, headers)
+
+            if not json.loads(response.text)['ok']:
+                send_message("#bottesting", "Error sending Slack message to " + chan + ' - \r\n' + response.text + ' @wombat3')
+
+        except Exception:
+            send_message("#bottesting", "General Error sending Slack message to " + chan + ' @wombat3')
 
     return
